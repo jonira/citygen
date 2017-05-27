cc35e79cd5875dc38b8a5c354f2e9b8f38cff9bc
Joni Rajanen <joni.rajanen@reaktor.com>
2017-05-27 21:02:08 +0300

    Normal distribution for angle and distance

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import * as PIXI from 'pixi.js'
-import Victor from 'victor'
 import * as R from 'ramda'
 
-const initVec = () => new Victor(50, 50)
+import Victor from 'victor'
+import gaussian from 'gaussian'
+
+const initVec = () => new Victor(100, 100)
+
+const distanceSampler = gaussian(100, 40)
+const angleSampler = gaussian(0, 295)
 
 const nextPoint = lastV => {
   const unit = new Victor(1, 0)
-  const dev = Math.PI * (Math.random() - .5)
-  const scale = 20 * Math.random()
-  return lastV.clone().add(unit.rotateBy(dev).multiply(new Victor(scale, scale)))
+  const dev = angleSampler.ppf(Math.random())
+  const scale = distanceSampler.ppf(Math.random())
+  return lastV.clone().add(unit.rotateByDeg(dev).multiply(new Victor(scale, scale)))
 }
 
 var app = new PIXI.Application()
@@ -34,16 +39,17 @@ const points = R.until(
     R.append(R.__, arr)
   )(arr))([startP])
 
-console.log('points', points)
 
-const endPoint = {x: 80, y: 80}
+console.log('points', points)
 
 const thickness = 2
 const graph = myGraph.lineStyle(thickness, 0xffffff)
   .moveTo(0, 0)
 
-R.map(p =>
-  graph.lineTo(p.x, p.y), points)
+R.pipe(
+  R.tail,
+  R.map(p =>
+    graph.lineTo(p.x, p.y)))(points)
 
 
 
