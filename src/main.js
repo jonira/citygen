66a705381735166d20f5554ed9ace226c1b3e5ce
Joni Rajanen <joni.rajanen@reaktor.com>
2017-05-27 23:30:39 +0300

    refactoring

--- src/main.js
+++ src/main.js
@@ -4,16 +4,37 @@ import * as R from 'ramda'
 import Victor from 'victor'
 import gaussian from 'gaussian'
 
-const initVec = () => new Victor(100, 100)
+const initVec = () => new Victor(0, 100)
 
-const distanceSampler = gaussian(100, 40)
+const distanceSampler = gaussian(40, 40)
 const angleSampler = gaussian(0, 295)
 
-const nextPoint = lastV => {
+const nextPoint = (distanceDist, angleDist, lastV) => {
   const unit = new Victor(1, 0)
-  const dev = angleSampler.ppf(Math.random())
-  const scale = distanceSampler.ppf(Math.random())
+  const dev = angleDist.ppf(Math.random())
+  const scale = distanceDist.ppf(Math.random())
   return lastV.clone().add(unit.rotateByDeg(dev).multiply(new Victor(scale, scale)))
+}
+
+const buildRoad = (graph, start, distanceDistribution, angleDistribution) => {
+  const segmentCount = 50
+  const thickness = 3
+  const points = R.until(
+    R.pipe(
+      R.length,
+      R.gt(R.__, segmentCount))
+    , arr => R.pipe(
+      R.last,
+      R.partial(nextPoint, [distanceDistribution, angleDistribution]),
+      R.append(R.__, arr)
+    )(arr))([startP])
+
+  graph.position.set(startP.x, startP.y)
+  graph.lineStyle(thickness, 0xffffff)
+    .moveTo(0, 0)
+  R.pipe(
+    R.map(p =>
+      graph.lineTo(p.x, p.y)))(points)
 }
 
 var app = new PIXI.Application()
@@ -25,31 +46,11 @@ app.stage.addChild(myGraph)
 
 const startP = initVec()
 
-myGraph.position.set(startP.x, startP.y)
-
-const segmentCount = 50
-
-const points = R.until(
-  R.pipe(
-    R.length,
-    R.gt(R.__, segmentCount))
-  , arr => R.pipe(
-    R.last,
-    nextPoint,
-    R.append(R.__, arr)
-  )(arr))([startP])
+buildRoad(myGraph, startP, distanceSampler, angleSampler)
 
 
-console.log('points', points)
 
-const thickness = 2
-const graph = myGraph.lineStyle(thickness, 0xffffff)
-  .moveTo(0, 0)
 
-R.pipe(
-  R.tail,
-  R.map(p =>
-    graph.lineTo(p.x, p.y)))(points)
 
 
 
