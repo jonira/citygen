918a7cb2a99961b3e0eb42ffe508489f4ab4f7b1
Joni Rajanen <joni.rajanen@reaktor.com>
2017-05-27 20:35:25 +0300

    Code style formatting

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
-const express = require("express");
+const express = require('express')
 const bodyParser = require('body-parser')
-
 
 const app = express()
 
@@ -11,5 +9,5 @@ app.use(bodyParser.json({limit: '5000kb'}))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log('server listening on port ', port)
+  console.log('server listening on port ', port)
 })
--- src/main.js
+++ src/main.js
@@ -1,49 +1,49 @@
-import * as PIXI from "pixi.js";
-import Victor from "victor";
-import * as R from "ramda";
+import * as PIXI from 'pixi.js'
+import Victor from 'victor'
+import * as R from 'ramda'
 
 const initVec = () => new Victor(50, 50)
 
 const nextPoint = lastV => {
-    const unit = new Victor(1,0)
-    const dev = Math.PI * (Math.random() - .5)
-    const scale = 20 * Math.random()
-    return lastV.clone().add(unit.rotateBy(dev).multiply(new Victor(scale, scale)))
+  const unit = new Victor(1, 0)
+  const dev = Math.PI * (Math.random() - .5)
+  const scale = 20 * Math.random()
+  return lastV.clone().add(unit.rotateBy(dev).multiply(new Victor(scale, scale)))
 }
 
-var app = new PIXI.Application();
+var app = new PIXI.Application()
 
-document.body.appendChild(app.view);
+document.body.appendChild(app.view)
 
-let myGraph = new PIXI.Graphics();
-app.stage.addChild(myGraph);
+let myGraph = new PIXI.Graphics()
+app.stage.addChild(myGraph)
 
 const startP = initVec()
 
-myGraph.position.set(startP.x, startP.y);
+myGraph.position.set(startP.x, startP.y)
 
 const segmentCount = 50
 
 const points = R.until(
-    R.pipe(
-        R.length,
-        R.gt(R.__, segmentCount))
-    , arr => R.pipe(
-        R.last,
-        nextPoint,
-        R.append(R.__, arr)
-    )(arr))([startP])
+  R.pipe(
+    R.length,
+    R.gt(R.__, segmentCount))
+  , arr => R.pipe(
+    R.last,
+    nextPoint,
+    R.append(R.__, arr)
+  )(arr))([startP])
 
 console.log('points', points)
 
-const endPoint = {x: 80, y: 80};
+const endPoint = {x: 80, y: 80}
 
 const thickness = 2
 const graph = myGraph.lineStyle(thickness, 0xffffff)
-    .moveTo(0, 0)
+  .moveTo(0, 0)
 
 R.map(p =>
-    graph.lineTo(p.x, p.y), points)
+  graph.lineTo(p.x, p.y), points)
 
 
 
