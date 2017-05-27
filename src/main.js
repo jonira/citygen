0ad44b788825a29ac14bd1ad6f7c2b1afbe2ac7e
Joni Rajanen <joni.rajanen@reaktor.com>
2017-05-27 20:33:51 +0300

    uniform random line

--- src/main.js
+++ src/main.js
@@ -1,21 +1,49 @@
-import * as PIXI from 'pixi.js'
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container.
+import * as PIXI from "pixi.js";
+import Victor from "victor";
+import * as R from "ramda";
+
+const initVec = () => new Victor(50, 50)
+
+const nextPoint = lastV => {
+    const unit = new Victor(1,0)
+    const dev = Math.PI * (Math.random() - .5)
+    const scale = 20 * Math.random()
+    return lastV.clone().add(unit.rotateBy(dev).multiply(new Victor(scale, scale)))
+}
+
 var app = new PIXI.Application();
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM.
 document.body.appendChild(app.view);
 
 let myGraph = new PIXI.Graphics();
 app.stage.addChild(myGraph);
 
-myGraph.position.set(10, 10);
+const startP = initVec()
+
+myGraph.position.set(startP.x, startP.y);
+
+const segmentCount = 50
+
+const points = R.until(
+    R.pipe(
+        R.length,
+        R.gt(R.__, segmentCount))
+    , arr => R.pipe(
+        R.last,
+        nextPoint,
+        R.append(R.__, arr)
+    )(arr))([startP])
+
+console.log('points', points)
 
 const endPoint = {x: 80, y: 80};
 
 const thickness = 2
-myGraph.lineStyle(thickness, 0xffffff)
+const graph = myGraph.lineStyle(thickness, 0xffffff)
     .moveTo(0, 0)
-    .lineTo(endPoint.x, endPoint.y);
+
+R.map(p =>
+    graph.lineTo(p.x, p.y), points)
+
+
+
